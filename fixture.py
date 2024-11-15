95764832e195c9e7437047caf2c58a5fbf0a33ac
johnmcgettrick <johnmcgettrick@gmail.com>
2024-11-15 12:41:55 +0000

    Add Team class and objects to fixture then process events to show cards

--- fixture.py
+++ fixture.py
@@ -1,17 +1,25 @@
 from datetime import datetime
 
+from team import Team
+
 class Fixture:
     id = 0
+    teams = {}
     kickoff = datetime.now()
     venue = ""
-    goals = {"home": 0, "away": 0}
-    cards = {"home": 0, "away": 0}
+    goals = {}
+    cards = {}
 
     def __init__(self, id, timestamp, data):
         self.id = id
         self.kickoff = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S")
         self.venue = data["fixture"]["venue"]["name"] if data["fixture"]["venue"] else ""
         
+        self.teams["home"] = Team(data["teams"]["home"]["id"], data["teams"]["home"]["name"])
+        self.teams["away"] = Team(data["teams"]["away"]["id"], data["teams"]["away"]["name"])
+
+        self.cards = {"home": 0, "away": 0}
+
         self.process_events(data)
 
     def process_events(self, data):
@@ -19,3 +27,7 @@ class Fixture:
             "home": int(data["goals"]["home"]) if data["goals"]["home"] else 0, 
             "away": int(data["goals"]["away"]) if data["goals"]["away"] else 0
         }
+        for ev in data["events"]:
+            if ev["type"] == "card":
+                team = "home" if ev["team"]["id"] == self.teams["home"].id else "away"
+                self.cards[team] += 1
---
+++ team.py
@@ -0,0 +1,7 @@
+class Team:
+    id = 0
+    name = ""
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
