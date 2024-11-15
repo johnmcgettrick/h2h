eb072d91cd1bc2a485f7b1686bfc52e83fc45419
johnmcgettrick <johnmcgettrick@gmail.com>
2024-11-15 12:42:32 +0000

    Add Test Data json files and cards test

--- tests/test_fixture.py
+++ tests/test_fixture.py
@@ -1,4 +1,7 @@
 import unittest
+import json
+import pathlib
+
 from datetime import datetime
 
 from fixture import Fixture
@@ -6,14 +9,29 @@ from fixture import Fixture
 class TestFixture(unittest.TestCase):
 
     def setUp(self):
-        self.fixture1 = Fixture(1, datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z"), {"fixture": { "venue": { "name": "The Park" } }, "goals":{"home": 3, "away": 1}})
-        self.fixture2 = Fixture(1, datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z"), {"fixture": { "venue": { "name": "El Stadio" } }, "goals":{"home": 2, "away": 0}})
+        file = pathlib.Path("tests/data/fixture1.json")  
+        with open(file) as f:  
+            data_fixture1 = json.load(f)
 
-    def test_process_events(self):
+        self.fixture1 = Fixture(1, datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z"), data_fixture1)
+
+        file = pathlib.Path("tests/data/fixture2.json")  
+        with open(file) as f:  
+            data_fixture2 = json.load(f)
+        
+        self.fixture2 = Fixture(2, datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z"), data_fixture2)
+
+    def test_process_events_score(self):
         self.assertEqual(self.fixture1.goals["home"], 3, "Home team goals for fixture 1 are set correctly")
         self.assertEqual(self.fixture1.goals["away"], 1, "Away team goals for fixture 1 are set correctly")
         self.assertEqual(self.fixture2.goals["home"], 2, "Home team goals for fixture 2 are set correctly")
         self.assertEqual(self.fixture2.goals["away"], 0, "Away team goals for fixture 2 are set correctly")
+
+    def test_process_events_cards(self):
+        self.assertEqual(self.fixture1.cards["home"], 1, "Home team cards for fixture 1 are set correctly")
+        self.assertEqual(self.fixture1.cards["away"], 2, "Away team cards for fixture 1 are set correctly")
+        self.assertEqual(self.fixture2.cards["home"], 0, "Home team cards for fixture 2 are set correctly")
+        self.assertEqual(self.fixture2.cards["away"], 0, "Away team cards for fixture 2 are set correctly")
 
 
 if __name__ == '__main__':
